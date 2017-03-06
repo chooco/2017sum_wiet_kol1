d376938daa858ac73656a2a1a2e0dd264593602e
chooco <kllaudiaa@gmail.com>
2017-03-06 16:08:41 +0100

    Update kol1.py

--- kol1.py
+++ kol1.py
@@ -3,3 +3,20 @@ The program should print out current orientation, and applied tilt correction.
 The program should run in infinite loop, until user breaks the loop. 
 Assume that plane orientation in every new simulation step is random angle with gaussian distribution (the planes is experiencing "turbulations"). 
 With every simulation step the orentation should be corrected, applied and printed out.
+
+#!/usr/bin/python
+import random
+
+class planePosition(position):
+
+ current_position = 0
+ correction = random.gauss(0,1)
+ correct_position = current_position - correction
+
+ return correct_position
+
+print "/n"
+
+while(True):
+ print "Current orientation: " + correct_position
+ print "Applied correction: " + correction 
